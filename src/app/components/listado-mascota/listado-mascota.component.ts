0d84add6100c797c415e926f0c5a532ffda03da0
Julio Davila <juliodacer@gmail.com>
2022-10-05 05:39:02 -0500

    filtro a la tabla

--- src/app/components/listado-mascota/listado-mascota.component.ts
+++ src/app/components/listado-mascota/listado-mascota.component.ts
@@ -1,5 +1,6 @@
 import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { Mascota } from 'src/app/interfaces/mascota';
 
@@ -25,6 +26,7 @@ export class ListadoMascotaComponent implements OnInit, AfterViewInit {
   dataSource = new MatTableDataSource<Mascota>(LIST_MASCOTAS);
 
   @ViewChild(MatPaginator) paginator!: MatPaginator //! --> No es nulo
+  @ViewChild(MatSort) sort!: MatSort //! --> No es nulo
 
   constructor() { }
 
@@ -33,7 +35,13 @@ export class ListadoMascotaComponent implements OnInit, AfterViewInit {
 
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
     this.paginator._intl.itemsPerPageLabel = 'items por página';
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
 }
